a72a7f1be2464a32feb035163dc0a6a7a23918d1
Copephobia <copephobia@gmail.com>
2015-07-23 20:24:39 -0400

    Initial HTML Template
    Header and footer files are created so that they may be easily
    manageable and do not distract the main PHP files from their intended
    functionality.

---
+++ src/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <title>Strategy Game</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div id="header">Strategy Game</div>
+        <div id="container">
+            <div id="nav">
+                <div id="nav_div">
+                    <!-- Navigation Menu -->
+                    
+                </div>
+            </div>
+            <div id="content">
+                <div id="con_div">
+                    <!-- Content -->
---
+++ src/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Index
+ * 
+ * This is the index page, where news, updates, or other
+ * such information can be presented.
+ * 
+ **/
+ 
+ /* Require the Header file */
+require_once("./header.php");
+
+
+
+/* Require the Footer file */
+require_once("./footer.php");
+?>
